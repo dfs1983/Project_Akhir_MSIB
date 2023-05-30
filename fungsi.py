299eac3db9efb22175f21eae3b1d66a9d1b364f4
adindaauliaa <130948141+adindaauliaa@users.noreply.github.com>
2023-05-30 10:49:51 +0700

    Update fungsi.py

--- fungsi.py
+++ fungsi.py
@@ -1,8 +1,17 @@
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, MaxPool2D, Flatten, Dense, Activation, Dropout,LeakyReLU
+from tensorflow.keras.utils import image_dataset_from_directory
+from tensorflow.data.experimental import AUTOTUNE
+from tensorflow.keras import Sequential, Input, Model
+from tensorflow.keras.layers import RandomRotation, RandomZoom
+from tensorflow.keras.layers.experimental.preprocessing import Rescaling
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Dropout
+from tensorflow.keras import applications
+from tensorflow.keras.losses import CategoricalCrossentropy
+from tensorflow.keras.optimizers import Adam
 
 def make_model():
+    BATCH_SIZE = 32
+    IMG_SIZE = (128, 128)
     IMG_SHAPE = image_shape + (3,)
     base_model = applications.EfficientNetB0(input_shape=IMG_SHAPE,
                                              include_top=False, 
