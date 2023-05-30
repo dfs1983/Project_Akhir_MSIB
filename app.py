69a1115d0829cbb391ad25ae06efa543f4383711
betanarulita <91450916+betanarulita@users.noreply.github.com>
2023-05-30 12:19:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -97,14 +97,16 @@ def apiDeteksi():
 # =[Main]========================================		
 
 if __name__ == '__main__':
-    IMG_SIZE = (128, 128)  # Define IMG_SIZE here
+    # Load model yang telah ditraining
     model = make_model()
-    model.call(tf.keras.Input(shape=(IMG_SIZE[0], IMG_SIZE[1], 3)))  # Use IMG_SIZE appropriately
+    
+    # Panggil model terlebih dahulu
+    model.call(tf.keras.Input(shape=(IMG_SIZE, IMG_SIZE, 3)))
+    
+    # Muat berat (weights) model
+    model.load_weights("modelwayang.h5")
 
-    # Load weights (if available)
-    model.load_weights("model_Wayang5_cnn_tf.h5")
-
-    # Run Flask on localhost
+    # Run Flask di localhost 
     run_with_ngrok(app)
     app.run()
 
