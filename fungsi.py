1fb6bc05475638775623441e73621e5f1d7c0eb6
adindaauliaa <130948141+adindaauliaa@users.noreply.github.com>
2023-05-30 10:25:56 +0700

    Update fungsi.py

--- fungsi.py
+++ fungsi.py
@@ -3,21 +3,22 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, MaxPool2D, Flatten, Dense, Activation, Dropout,LeakyReLU
 
 def make_model():
-    model = Sequential()
-    kernel = 3
-    size_w = 64
-    size_h = size_w
-    model.add(Conv2D(filters=64, kernel_size=kernel, input_shape=(size_w, size_h, 3), activation='relu'))
-    model.add(MaxPool2D(pool_size=(2, 2)))
-    model.add(Conv2D(filters=128, kernel_size=kernel, activation='relu'))
-    model.add(MaxPool2D(pool_size=(2, 2)))
-    model.add(Conv2D(filters=256, kernel_size=kernel, activation='relu'))
-    model.add(MaxPool2D(pool_size=(2, 2)))
-    model.add(Conv2D(filters=512, kernel_size=kernel, activation='relu'))
-    model.add(MaxPool2D(pool_size=(2, 2)))
-    model.add(Flatten())
-    model.add(Dense(256, activation='relu'))
-    model.add(Dropout(0.2))
-    model.add(Dense(20, activation='softmax'))
+    IMG_SHAPE = image_shape + (3,)
+    base_model = applications.EfficientNetB0(input_shape=IMG_SHAPE,
+                                             include_top=False, 
+                                             weights='imagenet')
+    
+    base_model.trainable = True
+    for layer in base_model.layers[0:218]:
+        layer.trainable = False
+    
+    inputs = Input(shape=IMG_SHAPE)
+    x = data_augmentation(inputs)
+    x = preprocess_input(inputs)
+    x = base_model(x)
+    x = GlobalAveragePooling2D()(x)
+    outputs = Dense(units=5, activation = "softmax")(x)
+    
+    model = Model(inputs, outputs)
     
     return model
